bb7b148b665baa9cb75c40960d4908e6a49a2727
Saskia van Barthold <saskia.vanb@gmail.com>
2020-06-10 17:16:38 +0200

    hard coded creating new client

--- db_clients_oop.py
+++ db_clients_oop.py
@@ -9,14 +9,17 @@ class DBClientstable(MSDBconnection):
     def get_clients(self):
         return self.sql_query('SELECT * FROM Customers')
 
+# print(clients_table.get_clients())
+
+    # def create_client(self):
+    #     q_result = self.sql_query("INSERT INTO Customers VALUES ('ZEIR', 'Saskia's shop', 'Saskia vB', 'Founder', '4    Privet Drive', 'Leavesden', 'NULL', 'WD25 7LR', 'UK', '24535', '24564756'")
+    #     return q_result
+
+    def create_entry(self, CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, ReorderLevel, Discontinued):
+        return self.sql_query(f"""INSERT INTO Customers (ProductID, ProductName,    SupplierID, CategoryID, QuantityPerUnit, UnitsInStock, UnitsOnOrder,    ReorderLevel, Discontinued)
+    VALUES (f{ProductID}, '{ProductName}', {SupplierID}, {CategoryI D},'{QuantityPerUnit}', {UnitsInStock}, {UnitsOnOrder}, {ReorderLevel}, {Discontinued})""")
+
 clients_table = DBClientstable()
-print(clients_table.get_clients())
-
-    def create_client(self, client_name):
-        q_result = self.sql_query("INSERT INTO Customers VALUES ('ZEIR', 'Saskia's shop', 'Saskia vB', 'Founder', '4 Privet Drive', 'Leavesden', 'NULL', 'WD25 7LR', 'UK', '24535', '24564756'")
-        return 
-
-
 # 2)
 # And then further abstract the interaction with specific Tables.
 #
