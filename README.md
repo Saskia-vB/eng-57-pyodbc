7a3319d5953efc631cc6e6bf187673a93cf42a83
Saskia van Barthold <saskia.vanb@gmail.com>
2020-06-10 11:53:05 +0200

    added a while loop and fetchmany, also updated README

--- README.md
+++ README.md
@@ -11,14 +11,23 @@ This package abstracts our connections to the DB.
 - Cursor is a method that represents a database cursor. 
 - Manages the context of a fetch operation 
 - Cursors created from the same connection are not isolated (changes done by one cursors are immediately visible to other cursors)
+- OOP: a method is a function that belongs to a object and a parameter/attribute of an object is a characterisitc, a variable that belongs to the object.
+- It keeps state: so like a stack of data, once it fetches one that one is 'removed' and it goes on to the next row.
 
-It keeps state.
-
-syntax: query_result = cursor.execute('SELECT * FROM Products')
+syntax: 
+query_result = cursor.execute('SELECT * FROM Products')
 print(query_result.fetchone())
-print(query_result.fetchall())
+data_point_card = query_result.fetchone()
+- behaves like an iterable list, organised with index
+print(data_point_card[1])
+- behave like OOP object (to complete)
+
+- Get all the rows:
+- syntax : print(query_result.fetchall())
+- bad because of the quantity of data
 
 all_results_list = query_result.fetchall()
 
 for data in all_results_list:
     print(data.ProductName, data.UnitPrice)
+    
--- db_connect.py
+++ db_connect.py
@@ -15,14 +15,33 @@ connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+ser
 cursor = connection.cursor()
 
 # print(cursor)
-query = cursor.execute('SELECT * FROM Products')
+query_result = cursor.execute('SELECT * FROM Products')
 
 # print(query.fetchall())
-rows = []
-while True:
-    row = cursor.fetchone()
-    rows.append(row)
-    if row == None:
-        break
-    print(row)
-print(rows[0])
+# rows = []
+# while True:
+#     row = cursor.fetchone()
+#     rows.append(row)
+#     if row == None:
+#         break
+#     print(row)
+# print(rows[0])
+
+# the better way of getting out all your data
+# while True:
+# # use our query results or cursor with query results and
+# # fetch one at time and
+# # do whatever we want/need with that data point - print it? get out the price? add a discount?
+# # stop the iteration when there is no more data
+# # -> or when the date point is None (python)/null (SQL)
+#     row = query_result.fetchone()
+#     if row == None:
+#         break
+#     print('NOW ONLY:', float(row.UnitPrice) * 1.25)
+
+
+# fetchmany when you know how many rows
+
+print(cursor.fetchmany(30))
+
+# the way you fetch data will have an impact on performance and price at the end of the month
